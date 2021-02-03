29b9eb33412eb691224c4055d52e66e2ff7328d3
brunoPercegaroli <bruno.percegaroli@gmail.com>
2021-02-02 21:29:58 -0300

    chore(Autenticao): Ajuste temporario de espacamento entre componentes

--- src/components/Login/Page/styles.ts
+++ src/components/Login/Page/styles.ts
@@ -16,4 +16,10 @@ export const Card = styled.div`
   border-radius: 18px;
   background: #393E46;
   box-shadow: 2px 4px 6px rgba(27, 27, 27, 0.2);
+  > div {
+    margin-right:  65px;
+    &:last-child {
+      margin-right: 0;
+    }
+  }
 `;
